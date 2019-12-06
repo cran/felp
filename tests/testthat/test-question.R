4fd76437b711dcdd86476bad581bdc008a2fd3ae
Atsushi Yasumoto <atusy.rpkg@gmail.com>
2019-12-06 14:40:02 +0000

    version 0.2.2

--- tests/testthat/test-question.R
+++ tests/testthat/test-question.R
@@ -1,10 +1,15 @@
 test_that("prefix operators", {
-  rmarkdown::render("test-question-prefix.Rmd", encoding = "UTF-8", quiet = TRUE)
+  knitr::knit("test-question-prefix.Rmd", encoding = "UTF-8", quiet = TRUE)
   expect_identical_except_bytecode("test-question-prefix.md", "ans.md")
 })
 
 
 test_that("pseudo postfix operators", {
-  rmarkdown::render("test-question-postfix.Rmd", encoding = "UTF-8", quiet = TRUE)
+  knitr::knit("test-question-postfix.Rmd", encoding = "UTF-8", quiet = TRUE)
   expect_identical_except_bytecode("test-question-postfix.md", "ans.md")
+})
+
+test_that("infix operator", {
+  # For better test, dummy S4 generic and method are required
+  expect_error(dummy_e1?dummy_e2)
 })
